a53bc96a15175b09398990e29eb76a3fbb912a6e
theViz343 <vishwesh103@gmail.com>
2020-03-06 19:55:26 +0530

    Changed display of latest post to cards

--- mysite/landingpage/views.py
+++ mysite/landingpage/views.py
@@ -6,11 +6,8 @@ from .models import BlogPost
 # Create your views here.
 def index(request) :
     latest_blogs=BlogPost.objects.order_by('-post_date')[:10]
-    new_blog=latest_blogs[0]
-    latest_blogs=[latest_blogs[1]]
     context={
         'blog_list':latest_blogs,
-        'new blog':new_blog,
              }
 
     return render(request, 'landingpage/index.html', context)
